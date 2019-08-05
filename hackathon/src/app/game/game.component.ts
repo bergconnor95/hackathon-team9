1bb0e91520fea929d523570d450d2cd18d8dcb5e
ryan <rse68380@ucmo.edu>
2019-08-05 16:41:31 -0500

    ryan's

--- hackathon/src/app/game/game.component.ts
+++ hackathon/src/app/game/game.component.ts
@@ -10,6 +10,14 @@ export class GameComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  }
 
+  }
+  barheight = 600;
+  earnButton = document.getElementById("Earn");
+  earn(){
+    this.barheight+=100;
+    document.getElementById("myBar").style.height=this.barheight+"px"
+    console.log(document.getElementById("myBar").style.height)
+    // window.scrollTo(0,document.body.scrollHeight)
+  }
 }
