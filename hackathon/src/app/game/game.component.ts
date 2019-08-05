b9ad294c3e2ad7390f9b8846ef9e7ea9f9b91437
Nicolle Lenzmeier <nalyv2@mail.missouri.edu>
2019-08-05 16:54:28 -0500

    Add modal service

--- hackathon/src/app/game/game.component.ts
+++ hackathon/src/app/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-game',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: ModalService) { }
 
   ngOnInit() {
+  }
+
+  openModal(id: string) {
+    console.log('I opened modal!');
+    this.modalService.open(id);
   }
 
 }
