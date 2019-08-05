4320c36809f82514f2931c54637ddfb1d1da615d
Connor Berg <bergconnor@gmail.com>
2019-08-05 14:17:56 -0500

    whitespace changes

--- backend/src/test/java/com/example/hackathon/HackathonApplicationTests.java
+++ backend/src/test/java/com/example/hackathon/HackathonApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class HackathonApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
