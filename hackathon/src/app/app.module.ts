2a28511775f5af473c2258ec2367e896291ae3e9
Connor Berg <bergconnor@gmail.com>
2019-08-05 13:43:31 -0500

    add button click incrementer

--- hackathon/src/app/app.module.ts
+++ hackathon/src/app/app.module.ts
@@ -6,18 +6,21 @@ import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
 import { GameComponent } from './game/game.component';
 import { HomeComponent } from './home/home.component';
+import { ButtonClickComponent } from './button-click/button-click.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'game', component: GameComponent}
+  {path: 'game', component: GameComponent},
+  {path: 'button-click', component: ButtonClickComponent}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
-    HomeComponent
+    HomeComponent,
+    ButtonClickComponent
   ],
   imports: [
     BrowserModule,
