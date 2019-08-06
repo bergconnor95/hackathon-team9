ea72204b44a087acb7388699c24dc5464e097bb9
Bobby <bsterling7717@gmail.com>
2019-08-06 09:07:23 -0500

    Event object initial commit

---
+++ backend/src/main/java/com/example/hackathon/model/event.java
@@ -0,0 +1,29 @@
+package com.example.hackathon.model;
+
+public class event {
+	private String name;
+	private String description;
+	private Double amount;
+	private boolean confirmTrigger;
+	private char positive;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public string getDescription() {
+		return description;
+	}
+	
+	public Double getAmount() {
+		return amount;
+	}
+	
+	public boolean getConfirmTrigger() {
+		return confirmTrigger;
+	}
+	
+	public char getPositive() {
+		return positive;
+	}
+}
